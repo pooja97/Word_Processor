7f319fb64b4d20a81e90ba553ff7e0620f3cdb38
pooja97 <poojayadav2124@gmail.com>
2022-12-10 18:33:27 -0800

    added runarray functionality

--- runarray.py
+++ runarray.py
@@ -1,3 +1,18 @@
 class RunArray:
+    #[start,end,font]
     def __init__(self):
-        pass
+        self.runArray = []  
+
+    #adding the font run with the start and end value in the runarray
+    def addRun(self,start,end,font):
+        fontlist = [font]*(end-start)
+        self.runArray[start:(end)] = fontlist
+    
+    #appending the font run at the end of the runarray
+    def appendRun(self,end,font):
+        fontlist = [font]*(end - len(self.runArray))
+        self.runArray.extend(fontlist)
+
+    def runArrayPrint(self):
+        print(self.runArray)
+
--- testcases.py
+++ testcases.py
@@ -1,6 +1,7 @@
 from character import Character
 from char_factory import CharFactory
 from font_factory import FontFactory
+from runarray import RunArray
 
 if __name__ == "__main__":
     charObj = Character()
@@ -33,3 +34,12 @@ if __name__ == "__main__":
 
     # print(font1)
     # print(font2)
+
+    test = RunArray()
+    test.addRun(0,2,'A')
+    test.appendRun(10,'B')
+    test.addRun(3,6,'C')
+    test.runArrayPrint()
+    test.appendRun(15,'D')
+    test.runArrayPrint()
+
